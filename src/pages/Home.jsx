1b19edd7382a130b5ee8ce6118b36427a3099e10
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-09-16 22:14:57 +0600

    create input field for search a post

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import PostCard from "../components/PostCard"
 import { useGetPostsQuery, useGetSpecificPostQuery } from "../redux/features/api/baseApi"
 
 function Home() {
+  // const {} =useForm
   // useGetPostsQuery return a object
   // const { isError, isLoading, data: posts } = useGetPostsQuery();
   const { isError, isLoading, data: post } = useGetSpecificPostQuery(1);
@@ -14,6 +15,10 @@ function Home() {
   return (
     <div className="container mx-auto">
       <h1>Home page</h1>
+      <form className="flex gap-3 py-10">
+        <input type="text" className="w-full rounded-md p-3"/>
+        <button type="submit" className="bg-zinc-800 text-zinc-300 rounded-md border border-zinc-500 px-3">Post</button>
+      </form>
       <div className="flex flex-col gap-3">
         {/* {posts?.map(post => <PostCard key={post.id} post={post}></PostCard>)} */}
         <PostCard post={post}/>
