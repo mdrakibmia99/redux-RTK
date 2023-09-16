c47de4b64df15ef3734c515f4a3e1e8dbbacca60
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-09-16 21:31:46 +0600

    create endpointsin baseAPi

--- src/redux/features/api/baseApi.js
+++ src/redux/features/api/baseApi.js
@@ -1,8 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-const baseApi=createApi({
-    reducerPath:"baseApi",
-    baseQuery:fetchBaseQuery({baseUrl:"https://jsonplaceholder.typicode.com/"}),
-    endpoints:{}
+const baseApi = createApi({
+    reducerPath: "baseApi",
+    baseQuery: fetchBaseQuery({ baseUrl: "https://jsonplaceholder.typicode.com" }),
+    endpoints: (builder) => ({
+        getPosts: builder.query({
+            query: () => '/posts'
+        }),
+    })
 });
-
+ export const {getPosts}=baseApi
 export default baseApi;
