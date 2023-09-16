1466e31a90008aa18b4bbc6ac9ded56616f044ab
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-09-16 21:46:49 +0600

    create postcart for map all posts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
+import PostCard from "../components/PostCard"
 import { useGetPostsQuery } from "../redux/features/api/baseApi"
 
 function Home() {
   // useGetPostsQuery return a object
-  const {isError,isLoading,data,error}=useGetPostsQuery()
+  const { isError, isLoading, data: posts, error } = useGetPostsQuery()
   return (
     <div>
-     <h1>Home page</h1>
+      <h1>Home page</h1>
+      <div className="flex flex-col gap-3">
+        {posts?.map(post => <PostCard key={post.id}></PostCard>)}
+      </div>
     </div>
   )
 }
