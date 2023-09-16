d5785e744ae9a7185d37c0aa6f0e646c7d06f9fa
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-09-16 22:18:01 +0600

    use useform for get data from form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
+import { useForm } from "react-hook-form";
 import PostCard from "../components/PostCard"
 import { useGetPostsQuery, useGetSpecificPostQuery } from "../redux/features/api/baseApi"
 
 function Home() {
-  // const {} =useForm
+  const {register,handleSubmit} =useForm()
   // useGetPostsQuery return a object
   // const { isError, isLoading, data: posts } = useGetPostsQuery();
   const { isError, isLoading, data: post } = useGetSpecificPostQuery(1);
+  const onSubmit = (data) => console.log(data)
+
   if(isLoading){
     return <p className="text-center text-9xl">Loading</p>
   }
@@ -15,8 +18,8 @@ function Home() {
   return (
     <div className="container mx-auto">
       <h1>Home page</h1>
-      <form className="flex gap-3 py-10">
-        <input type="text" className="w-full rounded-md p-3"/>
+      <form className="flex gap-3 py-10" onSubmit={handleSubmit(onSubmit)}>
+        <input {...register('post')} type="text" className="w-full rounded-md p-3"/>
         <button type="submit" className="bg-zinc-800 text-zinc-300 rounded-md border border-zinc-500 px-3">Post</button>
       </form>
       <div className="flex flex-col gap-3">
