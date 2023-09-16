efc7fbc44ca3a04f1aca3f89ff814e11566f6716
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-09-16 21:22:03 +0600

    api slice or baseAPi connect with store

--- src/redux/features/api/baseApi.js
+++ src/redux/features/api/baseApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 const baseApi=createApi({
-    reducerPath:"api",
+    reducerPath:"baseApi",
     baseQuery:fetchBaseQuery({baseUrl:"https://jsonplaceholder.typicode.com/"}),
     endpoints:{}
 });
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,5 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import baseApi from "./features/api/baseApi";
 const store = configureStore({
-    reducer: { }   
+    reducer: { 
+        [baseApi.reducerPath]: baseApi.reducer,
+    },
+     // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware().concat(baseApi.middleware),   
 });
 export default store;
