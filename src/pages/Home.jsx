96d7a2db71ad5bda5a515893abd605e5f0248e1d
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-09-16 21:53:40 +0600

    fetch data successfull

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,10 +5,10 @@ function Home() {
   // useGetPostsQuery return a object
   const { isError, isLoading, data: posts, error } = useGetPostsQuery()
   return (
-    <div>
+    <div className="container mx-auto">
       <h1>Home page</h1>
       <div className="flex flex-col gap-3">
-        {posts?.map(post => <PostCard key={post.id}></PostCard>)}
+        {posts?.map(post => <PostCard key={post.id} post={post}></PostCard>)}
       </div>
     </div>
   )
